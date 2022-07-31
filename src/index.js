dc1d37415e015ed695b27dbbf089d34d00cd39cd
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-07-31 18:55:03 -0500

    Obital Ring

--- src/index.js
+++ src/index.js
@@ -30,9 +30,22 @@ import anime from "https://cdn.skypack.dev/animejs@3.2.1";
 
 const scene = new Scene();
 
+let sunBackground = document.querySelector(".sun-background");
+let moonBackground = document.querySelector(".moon-background");
+
+const ringsScene = new Scene();
+
 // PerspectiveCamera(field of view, aspect ratio of screen, near plane i.e closest object to be rendered, far plane i.e furthest object to be rendered)
 const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(0, 15, 50);
+
+const ringsCamera = new PerspectiveCamera(
+  45,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+ringsCamera.position.set(0, 0, 50);
 
 const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(innerWidth, innerHeight);
@@ -65,6 +78,16 @@ sunLight.shadow.camera.top = 10;
 sunLight.shadow.camera.right = 10;
 scene.add(sunLight);
 
+let mousePos = new Vector2(0, 0);
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.clientX - innerWidth * 0.5;
+  let y = e.clientY - innerHeight * 0.5;
+
+  mousePos.x = x * 0.0003;
+  mousePos.y = y * 0.0003;
+});
+
 (async function () {
   let pmrem = new PMREMGenerator(renderer);
   let envmapTexture = await new RGBELoader()
@@ -85,15 +108,15 @@ scene.add(sunLight);
   // (http://creativecommons.org/licenses/by/4.0/).
   let plane = (await new GLTFLoader().loadAsync("assets/plane/scene.glb")).scene
     .children[0];
-    let planesData = [
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-      ];
+  let planesData = [
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+    makePlane(plane, textures.planeTrailMask, envMap, scene),
+  ];
 
   let sphere = new Mesh(
     new SphereGeometry(10, 70, 70),
@@ -115,10 +138,70 @@ scene.add(sunLight);
   sphere.receiveShadow = true;
   scene.add(sphere);
 
+  const ring1 = new Mesh(
+    new RingGeometry(15, 13.5, 80, 1, 0),
+    new MeshPhysicalMaterial({
+      color: new Color("#FFCB8E").convertSRGBToLinear().multiplyScalar(200),
+      roughness: 0.25,
+      envMap,
+      envMapIntensity: 1.8,
+      side: DoubleSide,
+      transparent: true,
+      opacity: 0.35,
+    })
+  );
+  ring1.name = "ring";
+  ring1.sunOpacity = 0.35;
+  ring1.moonOpacity = 0.03;
+  ringsScene.add(ring1);
+
+  const ring2 = new Mesh(
+    // RingGeometry(outerRadius, innerRadius, number of vertices)
+    new RingGeometry(16.5, 15.75, 80, 1, 0),
+    new MeshBasicMaterial({
+      color: new Color("#FFCB8E").convertSRGBToLinear(),
+      transparent: true,
+      opacity: 0.5,
+      side: DoubleSide,
+    })
+  );
+  ring2.name = "ring";
+  ring2.sunOpacity = 0.35;
+  ring2.moonOpacity = 0.1;
+  ringsScene.add(ring2);
+
+  const ring3 = new Mesh(
+    new RingGeometry(18, 17.75, 80),
+    new MeshBasicMaterial({
+      color: new Color("#FFCB8E").convertSRGBToLinear().multiplyScalar(50),
+      transparent: true,
+      opacity: 0.5,
+      side: DoubleSide,
+    })
+  );
+  ring3.name = "ring";
+  ring3.sunOpacity = 0.35;
+  ring3.moonOpacity = 0.03;
+  ringsScene.add(ring3);
+
   let clock = new Clock();
 
   renderer.setAnimationLoop(() => {
     let delta = clock.getDelta();
+
+    // Plane and Earth rendering
+    controls.update();
+    renderer.render(scene, camera);
+
+    // Rotate the ring by a certain amount (95% of previous rotation + 5% of current position of mousePos * a multiplyScalar)
+    ring1.rotation.x = ring1.rotation.x * 0.95 + mousePos.y * 0.05 * 1.2;
+    ring1.rotation.y = ring1.rotation.y * 0.95 + mousePos.x * 0.05 * 1.2;
+
+    ring2.rotation.x = ring2.rotation.x * 0.95 + mousePos.y * 0.05 * 0.375;
+    ring2.rotation.y = ring2.rotation.y * 0.95 + mousePos.x * 0.05 * 0.375;
+
+    ring3.rotation.x = ring3.rotation.x * 0.95 - mousePos.y * 0.05 * 0.275;
+    ring3.rotation.y = ring3.rotation.y * 0.95 - mousePos.x * 0.05 * 0.275;
 
     planesData.forEach((planeData) => {
       let plane = planeData.group;
@@ -152,8 +235,11 @@ scene.add(sunLight);
       // ------------------
     });
 
-    controls.update();
-    renderer.render(scene, camera);
+    // Ring camera rendering
+    // Must disable autoClear to render independent 3D scene
+    renderer.autoClear = false;
+    renderer.render(ringsScene, ringsCamera);
+    renderer.autoClear = true;
   });
 })();
 
@@ -176,8 +262,29 @@ function makePlane(planeMesh, trailTexture, envMap, scene) {
     }
   });
 
+  let trail = new Mesh(
+    new PlaneGeometry(1, 2),
+    new MeshPhysicalMaterial({
+      envMap,
+      envMapIntensity: 3,
+
+      roughness: 0.4,
+      metalness: 0,
+      transmission: 1,
+
+      transparent: true,
+      opacity: 1,
+      alphaMap: trailTexture,
+    })
+  );
+  //   trail.sunEnvIntensity = 3;
+  //   trail.moonEnvIntensity = 0.7;
+  trail.rotateX(Math.PI);
+  trail.translateY(1.1);
+
   let group = new Group();
   group.add(plane);
+  group.add(trail);
 
   scene.add(group);
 
